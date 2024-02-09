a5495255d4e2b7b9eaecda56a677cda0783386f2
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2024-02-09 08:14:25 -0300

    feat: layout do app com cabeçalho

---
+++ src/components/header.tsx
@@ -0,0 +1,28 @@
+import { HomeIcon, PizzaIcon, UtensilsCrossedIcon } from 'lucide-react'
+
+import { NavLink } from './nav-link'
+import { Separator } from './ui/separator'
+
+export function Header() {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center gap-6 px-6">
+        <PizzaIcon className="h-6 w-6" />
+
+        <Separator orientation="vertical" className="h-6" />
+
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+          <NavLink to={'/'}>
+            <HomeIcon className="h-4 w-4" />
+            Início
+          </NavLink>
+
+          <NavLink to={'/orders'}>
+            <UtensilsCrossedIcon className="h-4 w-4" />
+            Pedidos
+          </NavLink>
+        </nav>
+      </div>
+    </div>
+  )
+}
