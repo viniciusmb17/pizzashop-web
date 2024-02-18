03bf6d58029eb16189e3b4c1949f8b7b408bc99a
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2024-02-18 16:01:20 -0300

    feat: atualizando http state

--- src/api/get-managed-restaurant.ts
+++ src/api/get-managed-restaurant.ts
@@ -1,6 +1,6 @@
 import { api } from '@/lib/axios'
 
-interface GetManagedRestaurantResponse {
+export interface GetManagedRestaurantResponse {
   name: string
   id: string
   description: string | null
